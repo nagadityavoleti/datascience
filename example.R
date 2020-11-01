c884fd9e2cb281010063275f4ef7d728775b21e2
nagadityavoleti <73770869+nagadityavoleti@users.noreply.github.com>
2020-11-01 11:59:22 +0530

    Add files via upload

---
+++ example.R
@@ -0,0 +1,37 @@
+1+1
+x <-10
+y <- 20
+z <-30
+x*y*z
+x <- c(2,0,0,4)
+y <- c(1,9,9,9)
+x+y
+x*4
+round(sqrt(x),2)
+x[1]
+x[-1]
+x[1] <-3
+x
+x[-1]=5
+x
+y<9
+y[4]
+y<9
+y
+y
+y[y<9]=2
+y
+df <- data.frame(x=1:3,y=c('a', 'b', 'c'))
+df
+str(df)
+df_new <- data.frame(height=c(150,160), weight=c(65,72))
+df_new
+str(df_new)#structure of dataset
+df[1,1]
+df[1,]
+df[c(1,3),2]
+df[c(1,3),1]
+df[c(1,3),c(1,2)]
+df[c(1,3),]
+
+
